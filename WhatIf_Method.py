c3b10d134cbc8c0e2e3bbc33d24f0f13f5314f1e
Nabuiltdiff <124265956+Nabuiltdiff@users.noreply.github.com>
2024-03-26 21:14:38 +0800

    first commit

---
+++ WhatIf_Method.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from openai import OpenAI
+
+class WhatIfMethod:
+    @staticmethod
+    def whatif_ai(msg, client):
+        whatif_response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a what if generator. You tell users what if scenarios based on their input."
+                },
+                {
+                    "role": "user",
+                    "content": f"{msg}" 
+                }
+            ],
+            max_tokens=200,  
+            temperature=0.9
+        )
+
+        whatif = whatif_response.choices[0].message.content
+
+        return whatif
+
+    @staticmethod
+    def pic_ai(msg, client):
+        cover_response = client.images.generate(
+            model="dall-e-2",
+            prompt=f"{msg}",
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+
+        image_url = cover_response.data[0].url
+
+        return image_url
---
+++ Whatif_ai.py
@@ -0,0 +1,43 @@
+import os 
+import streamlit as st
+from openai import OpenAI
+from IPython.display import Image
+from WhatIf_Method import WhatIfMethod as WhatIfMethod
+
+
+def main():
+
+    with st.sidebar:
+        st.title("About App")
+        st.write("""The What If Generator is your brainstorming buddy!  This app helps you explore possibilities, overcome creative roadblocks, and  approach challenges from new angles. Simply enter a situation or question  and get a random "what if" scenario to jumpstart your thinking.
+
+Let's see where your imagination takes you!""")
+    
+    st.title("🤖 What If Generator with OpenAI")
+
+    # Category Selection
+    categories = ["Technology", "History", "Society", "Personal"]
+    selected_category = st.selectbox("Choose a Category:", categories)
+
+    user_input_msg = st.text_input("Enter a 'What if' scenario:", value=" What if humans could fly? e.g.")
+
+    if st.button("Generate Response"):
+        # Set the OpenAI API key
+        OpenAI.api_key = st.secrets["OPENAI_API_KEY"]
+        # Create an instance of the OpenAI class
+        client = OpenAI()
+        
+        whatif_response = WhatIfMethod.whatif_ai(user_input_msg, client)
+        st.write("Generated Text Response:")
+        st.write(whatif_response)
+
+        image_url = WhatIfMethod.pic_ai(user_input_msg, client)
+        st.write("Generated Image URL:")
+        st.write(image_url)
+
+if __name__ == "__main__":
+    main()
+
+
+
+    
